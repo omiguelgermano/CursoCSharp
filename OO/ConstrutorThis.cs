afcefff0d719a63624ef1e34cd96fab769405276
omiguelgermano <miguelgermano86@gmail.com>
2023-04-03 13:09:24 -0300

    Criação da Class Generica.

--- OO/ConstrutorThis.cs
+++ OO/ConstrutorThis.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CursoCSharp.OO {
+
+    public class Animal { // CLASSE GENERICA
+        public string Nome { get; set; }
+        public Animal(string nome) { // CONTRUTOR PAI
+            Nome = nome;
+        }
+    }
     internal class ConstrutorThis {
 
         public static void Executar() {
-
             Console.WriteLine("Construtor usando o This");
+
         }
     }
 }
