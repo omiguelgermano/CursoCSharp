1c754a3871459bd38007e2c675f5851b60e2fd68
omiguelgermano <miguelgermano86@gmail.com>
2023-04-07 02:16:40 -0300

    Criação de novo Projeto EncapsulamentoNew

--- OO/Encapsulamento.cs
+++ OO/Encapsulamento.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Encapsulamento;
+using EncapsulamentoNew;
 
 namespace CursoCSharp.OO {
 
     public class FilhoNaoReconhecido : SubCelebridade {
         public new void MeusAcessos() {
+            Console.WriteLine("FilhoNaoReconhecido...");
 
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            //Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            //Console.WriteLine(SegredoFamilia);
+            //Console.WriteLine(UsaMuitoPhotoshop);
         }
     }
     internal class Encapsulamento {
