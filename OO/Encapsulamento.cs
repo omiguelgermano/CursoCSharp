6217107bcf93169e267026fd387ecb5d0245ab69
omiguelgermano <miguelgermano86@gmail.com>
2023-04-07 02:29:10 -0300

    Encapsulamento com usando projetos externo.

--- OO/Encapsulamento.cs
+++ OO/Encapsulamento.cs
@@ -19,8 +19,32 @@ namespace CursoCSharp.OO {
             //Console.WriteLine(UsaMuitoPhotoshop);
         }
     }
+
+    public class AmigoDistante {
+        public readonly SubCelebridade amiga = new SubCelebridade();
+        public new void MeusAcessos() {
+            Console.WriteLine("AmigoDistante...");
+
+            Console.WriteLine(amiga.InfoPublica);
+            //Console.WriteLine(amiga.CorDoOlho);
+            //Console.WriteLine(amiga.NumeroCelular);
+            //Console.WriteLine(amiga.JeitoDeFalar);
+            //Console.WriteLine(amiga.SegredoFamilia);
+            //Console.WriteLine(amiga.UsaMuitoPhotoshop);
+
+        }
+    }
     internal class Encapsulamento {
 
-        public static void Executar() { }
+        public static void Executar() {
+        SubCelebridade sub = new SubCelebridade();
+            sub.MeusAcessos();
+
+            new FilhoNaoReconhecido().MeusAcessos();
+            new AmigoProximo().MeusAcessos();
+
+            new FilhoNaoReconhecido().MeusAcessos();
+            new AmigoDistante().MeusAcessos();
+        }
     }
 }
